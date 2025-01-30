56704446723fe1776c9f3e31fa0cfe60e69aacb7
Shahariar Mithu <kmmithu2015@gmail.com>
2025-01-30 21:03:17 +0600

    add registration design with functionality

---
+++ src/app/(auth)/signup/actions.ts
@@ -0,0 +1,81 @@
+"use server";
+
+import { lucia } from "@/auth";
+import prisma from "@/lib/prisma";
+import { signUpSchema, SignUpType } from "@/lib/validation";
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signup(
+  credentials: SignUpType
+): Promise<{ error: string }> {
+  try {
+    const { username, email, password } = signUpSchema.parse(credentials);
+
+    const passwordHash = await hash(password, {
+      memoryCost: 19456,
+      parallelism: 1,
+      timeCost: 2,
+      outputLen: 32,
+    });
+
+    const userId = generateIdFromEntropySize(10);
+
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUsername) {
+      return { error: "Username is already taken" };
+    }
+
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmail) {
+      return { error: "Email is already taken" };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        passwordHash,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+
+    const sessionCookie = lucia.createSessionCookie(session.id);
+
+    (await cookies()).set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+
+    console.error(error);
+
+    return { error: "An error occurred. Please try again." };
+  }
+}
